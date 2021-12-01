81c96e53a4fe16cb0bde6aa808f13dd76966cbbd
David Goosen <david@davidgoosen.kiwi>
2021-12-01 18:39:31 +0000

    Cleanup day 1 part a

--- day1/day1.hs
+++ day1/day1.hs
@@ -1,14 +1,15 @@
---import System.IO
+import System.IO
 
-depths2 = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-larger_than_previous :: [Integer] -> Int
-larger_than_previous depths =
-    let compare x = fst x > snd x
-        results = map compare (zip (tail depths) depths)
+testDepths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+largerThanPrevious :: [Int] -> Int
+largerThanPrevious depths =
+    let compare = uncurry (>)
+        results = zipWith (curry compare) (tail depths) depths
     in length (filter (==True) results)
 
--- main = do
---    handle <- openFile "input.txt" ReadMode
---    contents <- hGetContents handle
---    let values = lines contents
---    map putStrLn values
+main = do
+    handle <- openFile "input.txt" ReadMode
+    contents <- hGetContents handle
+    let readInt x = read x :: Int
+        values = map readInt (lines contents)
+    print (largerThanPrevious values)
