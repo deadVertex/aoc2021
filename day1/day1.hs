d7f334d15e2fb679f184143a0e200bbc3a04c5b6
David Goosen <david@davidgoosen.kiwi>
2021-12-02 07:10:39 +0000

    Finish second part of day 1

--- day1/day1.hs
+++ day1/day1.hs
@@ -1,15 +1,19 @@
 import System.IO
 
-testDepths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+testDepths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263] :: [Int]
 largerThanPrevious :: [Int] -> Int
 largerThanPrevious depths =
     let compare = uncurry (>)
         results = zipWith (curry compare) (tail depths) depths
     in length (filter (==True) results)
 
+slidingWindowSum :: [Int] -> [Int]
+slidingWindowSum [] = []
+slidingWindowSum xs = sum (take 3 xs) : slidingWindowSum (tail xs)
+
 main = do
     handle <- openFile "input.txt" ReadMode
     contents <- hGetContents handle
     let readInt x = read x :: Int
         values = map readInt (lines contents)
-    print (largerThanPrevious values)
+    print (largerThanPrevious (slidingWindowSum values))
