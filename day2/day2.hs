129f66f0819bae527d8258e7d3da74c25faf83b2
David Goosen <david@davidgoosen.kiwi>
2021-12-03 23:04:49 +0000

    Start modifying code for day 2 part 2, unit tests saving my butt

--- day2/day2.hs
+++ day2/day2.hs
@@ -5,25 +5,26 @@ convert :: [String] -> (String, Int)
 convert xs = 
     (head xs, read (last xs) :: Int)
 
-delta :: (String, Int) -> (Int, Int)
-delta ("forward", a) = (a, 0)
-delta ("up", a) = (0, -a)
-delta ("down", a) = (0, a)
+command :: (String, Int) -> (Int, Int, Int)
+command ("forward", a) = (a, 0, 0)
+command ("up", a) = (0, 0, -a)
+command ("down", a) = (0, 0, a)
 
-process :: [String] -> [(Int, Int)]
+process :: [String] -> [(Int, Int, Int)]
 process xs = 
     let tokens = map words xs
         actions = map convert tokens
-    in map delta actions
+    in map command actions
 
-sumComponents :: [(Int, Int)] -> (Int, Int)
+sumComponents :: [(Int, Int, Int)] -> (Int, Int, Int)
 sumComponents xs =
-    let a = unzip xs
-    in (sum (fst a), sum (snd a))
+    let a = [ x | (x,y,z) <- xs]
+        b = [ z | (x,y,z) <- xs]
+    in (sum a, sum b, 0)
 
 computeAnswer :: [String] -> Int
 computeAnswer xs =
-    let (a, b) = sumComponents (process xs)
+    let (a, b, _) = sumComponents (process xs)
     in a * b
 
 -- Application
@@ -35,11 +36,11 @@ main = do
 -- Unit tests and test input
 testInput = ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
 
-test1 = TestCase (assertEqual "delta (forward, 5)" (5, 0) (delta ("forward", 5)))
-test2 = TestCase (assertEqual "delta (down, 5)" (0, 5) (delta ("down", 5)))
-test3 = TestCase (assertEqual "delta (up, 3)" (0, -3) (delta ("up", 3)))
-test4 = TestCase (assertEqual "process" [(0,2)] (process ["down 2"]))
-test5 = TestCase (assertEqual "sumComponents" (4, 6) (sumComponents [(1,2), (3,4)]))
+test1 = TestCase (assertEqual "command (forward, 5)" (5, 0, 0) (command ("forward", 5)))
+test2 = TestCase (assertEqual "command (down, 5)" (0, 0, 5) (command ("down", 5)))
+test3 = TestCase (assertEqual "command (up, 3)" (0, 0, -3) (command ("up", 3)))
+test4 = TestCase (assertEqual "process" [(0, 0, 2)] (process ["down 2"]))
+test5 = TestCase (assertEqual "sumComponents" (4, 6, 0) (sumComponents [(1, 0, 2), (3, 0, 4)]))
 test6 = TestCase (assertEqual "computeAnswer" 150 (computeAnswer testInput))
 
 
