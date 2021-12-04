7f85e2741830089089496c0045c065e7a758a6ee
David Goosen <david@davidgoosen.kiwi>
2021-12-04 10:03:11 +0000

    Complete day 2 part 2

--- day2/day2.hs
+++ day2/day2.hs
@@ -5,27 +5,26 @@ convert :: [String] -> (String, Int)
 convert xs = 
     (head xs, read (last xs) :: Int)
 
-command :: (String, Int) -> (Int, Int, Int)
-command ("forward", a) = (a, 0, 0)
-command ("up", a) = (0, 0, -a)
-command ("down", a) = (0, 0, a)
+data Coordinate = Coordinate { horizontal :: Int
+                             , depth :: Int
+                             , aim :: Int
+                             } deriving (Show, Eq)
 
-process :: [String] -> [(Int, Int, Int)]
-process xs = 
-    let tokens = map words xs
-        actions = map convert tokens
-    in map command actions
+applyCommand :: (String, Int) -> Coordinate -> Coordinate
+applyCommand ("forward", a) x = Coordinate (horizontal x + a) (depth x + aim x * a) (aim x)
+applyCommand ("up", a) x = Coordinate (horizontal x) (depth x) (aim x - a)
+applyCommand ("down", a) x = Coordinate (horizontal x) (depth x) (aim x + a)
 
-sumComponents :: [(Int, Int, Int)] -> (Int, Int, Int)
-sumComponents xs =
-    let a = [ x | (x,y,z) <- xs]
-        b = [ z | (x,y,z) <- xs]
-    in (sum a, sum b, 0)
+sumCommands :: [(String, Int)] -> Coordinate -> Coordinate
+sumCommands [] y = y
+sumCommands xs y = sumCommands (tail xs) (applyCommand (head xs) y)
 
 computeAnswer :: [String] -> Int
 computeAnswer xs =
-    let (a, b, _) = sumComponents (process xs)
-    in a * b
+    let tokens = map words xs
+        actions = map convert tokens
+        x = sumCommands actions (Coordinate 0 0 0)
+    in horizontal x * depth x
 
 -- Application
 main = do
@@ -36,17 +35,17 @@ main = do
 -- Unit tests and test input
 testInput = ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
 
-test1 = TestCase (assertEqual "command (forward, 5)" (5, 0, 0) (command ("forward", 5)))
-test2 = TestCase (assertEqual "command (down, 5)" (0, 0, 5) (command ("down", 5)))
-test3 = TestCase (assertEqual "command (up, 3)" (0, 0, -3) (command ("up", 3)))
-test4 = TestCase (assertEqual "process" [(0, 0, 2)] (process ["down 2"]))
-test5 = TestCase (assertEqual "sumComponents" (4, 6, 0) (sumComponents [(1, 0, 2), (3, 0, 4)]))
-test6 = TestCase (assertEqual "computeAnswer" 150 (computeAnswer testInput))
+test1 = TestCase (assertEqual "command (forward, 2)" (Coordinate 4 1 0) (applyCommand ("forward", 2) (Coordinate 2 1 0)))
+test2 = TestCase (assertEqual "command (down, 3)" (Coordinate 2 1 5) (applyCommand ("down", 3) (Coordinate 2 1 2)))
+test3 = TestCase (assertEqual "command (up, 1)" (Coordinate 2 1 1) (applyCommand ("up", 1) (Coordinate 2 1 2)))
+test4 = TestCase (assertEqual "sumCommands" (Coordinate 5 0 0) (sumCommands [("forward", 1), ("forward", 3)] (Coordinate 1 0 0)))
+test5 = TestCase (assertEqual "computeAnswer" 900 (computeAnswer testInput))
 
+test6 = TestCase (assertEqual "command (forward, 2) with aim" (Coordinate 4 5 2) (applyCommand ("forward", 2) (Coordinate 2 1 2)))
 
 tests = TestList [TestLabel "test1" test1,
                   TestLabel "test2" test2,
                   TestLabel "test3" test3,
                   TestLabel "test4" test4,
                   TestLabel "test5" test5,
-                  TestLabel "test6" test6 ]
+                  TestLabel "test6" test6]
